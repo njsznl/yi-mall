6b24e5e8a70cfbeed5032819a0140f0151b69f46
伊老克拉 <30685635+njsznl@users.noreply.github.com>
2022-09-10 17:18:43 +0800

    添加nacos注册中心

--- mall-product/src/main/java/com/yi/mall/product/MallProductApplication.java
+++ mall-product/src/main/java/com/yi/mall/product/MallProductApplication.java
@@ -3,10 +3,12 @@ package com.yi.mall.product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author yi
  */
+@EnableDiscoveryClient
 @MapperScan("com.yi.mall.product.dao")
 @SpringBootApplication
 public class MallProductApplication {
